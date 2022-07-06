ef17066276049d9132d659d46637860fbe563d37
mackaylarodriguez <91747378+mackaylarodriguez@users.noreply.github.com>
2022-07-05 20:36:12 -0700

    Update main.java

--- main.java
+++ main.java
@@ -3,8 +3,13 @@ import javax.swing.JFrame;
 public class Main {
 	
 	public static void main(String[] args) {
-		JFrame frame = new JFrame();
-      		frame.setSize(420,420);
-      		frame.setVisible(true);
+		JFrame frame = new JFrame(); //creates frame
+      		frame.setTitle("MACS GUI"); //title of frame
+      		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //exit out of application;
+      		//DO_NOTHING_ON_CLOSE; they can not close out of gui
+      		frame.setResizable(false); //dont allow frame from being resized
+      		frame.setSize(420,420); //size of the frame
+      		frame.setVisible(true); //make frame visible
+      		frame.getContentPane().setBackground(Color.blue);
 	}
 }
