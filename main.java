e8bfd500c0827f140511388c14e0b63c7f8a6acb
mackaylarodriguez <91747378+mackaylarodriguez@users.noreply.github.com>
2022-07-05 19:26:12 -0700

    Update main.java

--- main.java
+++ main.java
@@ -4,8 +4,7 @@ public class Main {
 	
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
-      frame.setSize(420,420);
-      frame.setVisible(true);
-      
+      		frame.setSize(420,420);
+      		frame.setVisible(true);
 	}
 }
